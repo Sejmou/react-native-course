aebb7064b66f51f95ed3adc713c4620d42f40dff
Sejmou <samo.kolter@gmail.com>
2022-09-09 13:44:09 +0200

    improve styling

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
 
 export default function App() {
   return (
@@ -10,22 +10,33 @@ export default function App() {
         ></TextInput>
         <Button title="Add Goal" />
       </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals will come here</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1, //appContainer is "implicitly wrapped" by yet another flex container; set flex to 1 -> takes whole device height
+    paddingHorizontal: 16,
+    paddingTop: 32,
   },
   inputContainer: {
+    flex: 0,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
     flex: 2,
     marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 1,
   },
 });
