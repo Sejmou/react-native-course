f50658ec4dfe2cd6386533d3e19f3fe837d8cc33
Sejmou <samo.kolter@gmail.com>
2022-09-09 13:22:24 +0200

    styling with flexbox

--- App.js
+++ App.js
@@ -1,30 +1,31 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <View>
-        <Text style={styles.text}>
-          This Text element is styled with styles from a Stylesheet object
-        </Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal"
+        ></TextInput>
+        <Button title="Add Goal" />
       </View>
-      <Button title="Tap here!" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
   },
-  text: {
-    margin: 16,
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
     borderWidth: 1,
-    borderColor: 'red',
-    padding: 16,
+    borderColor: '#cccccc',
+    flex: 2,
+    marginRight: 8,
   },
 });
