b984e50eef23cac262ee7bc4ad92c5ccfd031c7b
Sejmou <samo.kolter@gmail.com>
2022-09-16 11:54:41 +0200

    add props + styles for number input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,17 @@
-import { TextInput, View, StyleSheet } from 'react-native';
+import { TextInput, View, StyleSheet, Platform } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        selectionColor={Platform.OS === 'ios' ? '#ddb52f' : null} // yellow selection looks really bad on Android, so...
+        cursorColor="#ddb52f" // ... use this for styling cursor on Android - text selection will remain in default OS color unfortunately
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,5 +33,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 48,
+    width: 48,
+    fontSize: 32,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    marginVertical: 8,
   },
 });
