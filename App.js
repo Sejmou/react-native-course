743be94374b3f02e2925c60d8e468b0d7666b595
Sejmou <samo.kolter@gmail.com>
2022-09-16 12:26:43 +0200

    adding background for each screen

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
+import { View, StyleSheet } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f',
+  },
+});
